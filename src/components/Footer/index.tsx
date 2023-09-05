c5d89c05d209790157ad2ea0febe86785886b1d0
Albertoseiji <alberto.assatoaberto@gmail.com>
2023-09-05 11:54:17 -0300

    Criando componente Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import{ Container } from './styles'
+
+const Footer: React.FC = () => {
+    return(
+        <Container>
+            <h1> Alberto Seiji Assato - RM: 99977 - Turma 1TDSPV</h1>
+        </Container>
+    )
+}
+export { Footer }
